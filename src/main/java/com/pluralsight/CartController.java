e66b7e698f9a160543226775f8931b4128fee8d2
Michael J Cox <mike.jon.cox@gmail.com>
2019-05-21 09:24:36 -0600

    added module 2 code

--- src/main/java/com/pluralsight/CartController.java
+++ src/main/java/com/pluralsight/CartController.java
@@ -1,89 +1,101 @@
 package com.pluralsight;
 
-import java.io.IOException;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.inject.Inject;
+import java.io.IOException;
+
 /**
  * Servlet implementation class HelloWorld
  */
 
 public class CartController extends HttpServlet {
-		private static final long serialVersionUID = 1L;
-		private DBConnection dbConnection;
+    private static final long serialVersionUID = 1L;
+    private DBConnection dbConnection;
 
-		@Inject
+    @Inject
     private BookDAO bookDAO;
-
-    public void init() {
-			dbConnection = new DBConnection();
-			bookDAO = new BookDAO(dbConnection.getConnection());
-    }
-
-		public void destroy() {
-			dbConnection.disconnect();
-		}
 
     public CartController() {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException  {
-		// The requested URL path
-		String action = request.getPathInfo();
+    public void init() {
+        dbConnection = new DBConnection();
+        bookDAO = new BookDAO(dbConnection.getConnection());
+    }
 
-		// Do different things depending on the action (or path requested)
-		try {
-			switch(action) {
-				case "/addcart":
-					 addToCart(request, response);
-           break;
-        default:
-           break;
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    public void destroy() {
+        dbConnection.disconnect();
+    }
 
-		response.sendRedirect("../ShoppingCart.jsp");
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // The requested URL path
+        String action = request.getPathInfo();
 
-  protected void addToCart(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-   HttpSession session = request.getSession();
-   String idStr = request.getParameter("id");
-   int id = Integer.parseInt(idStr);
-   String quantityStr = request.getParameter("quantity");
-   int quantity = Integer.parseInt(quantityStr);
+        // Do different things depending on the action (or path requested)
+        try {
+            switch (action) {
+                case "/addcart":
+                    addToCart(request, response);
+                    break;
+                case "/delete":
+                    deleteFromCart(request, response);
+                    break;
+                default:
+                    break;
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-	 // Get the book from the database
-   Book existingBook = bookDAO.getBook(id);
+        response.sendRedirect("../ShoppingCart.jsp");
+    }
 
-	 // Check if a ShoppingCart exists in the Session variable
-	 // If not create one
-   ShoppingCart shoppingCart = null;
-   Object objCartBean = session.getAttribute("cart");
+    protected void addToCart(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String idStr = request.getParameter("id");
+        int id = Integer.parseInt(idStr);
+        String quantityStr = request.getParameter("quantity");
+        int quantity = Integer.parseInt(quantityStr);
 
-   if(objCartBean!=null) {
-    shoppingCart = (ShoppingCart) objCartBean ;
-   } else {
-    shoppingCart = new ShoppingCart();
-    session.setAttribute("cart", shoppingCart);
-   }
+        // Get the book from the database
+        Book existingBook = bookDAO.getBook(id);
 
-	 // Add this item and quantity to the ShoppingCart
-   shoppingCart.addCartItem(existingBook, quantity);
-  }
+        // Check if a ShoppingCart exists in the Session variable
+        // If not create one
+        ShoppingCart shoppingCart = null;
+        Object objCartBean = session.getAttribute("cart");
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+        if (objCartBean != null) {
+            shoppingCart = (ShoppingCart) objCartBean;
+        } else {
+            shoppingCart = new ShoppingCart();
+            session.setAttribute("cart", shoppingCart);
+        }
+
+        // Add this item and quantity to the ShoppingCart
+        shoppingCart.addCartItem(existingBook, quantity);
+    }
+
+    private void deleteFromCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        int id = Integer.parseInt(request.getParameter("index"));
+        HttpSession sesh = request.getSession();
+        ShoppingCart cart = (ShoppingCart) sesh.getAttribute("cart");
+        cart.deleteCartItem(id);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
